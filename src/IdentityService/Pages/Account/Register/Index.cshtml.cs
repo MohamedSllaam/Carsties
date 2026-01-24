fe6d64a2c737b961ff49c634d181f59d01ee901f
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2026-01-25 01:06:08 +0200

    add register

--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -1,11 +1,63 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IdentityService.Pages.Account.Register
-{
+namespace IdentityService.Pages.Account.Register;
+
+[SecurityHeaders]
+[AllowAnonymous]
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+ private readonly UserManager<ApplicationUser> _usermanager;
+
+ public IndexModel(Usermanager<ApplicationUser> usermanager)
+    {
+    _usermanager = usermanager;
     }
-}
+
+    [BindProperty]
+     public RegisterViewModel Input { get; set; }
+
+     [BindProperty]
+     public bool RegisterSuccess { get; set;}
+        
+        public IActionResult OnGet(string returnUrl = null)
+         {
+         Input = new RegisterViewModel 
+         {
+             ReturnUrl = returnUrl
+         };
+     
+        return Page();
+         }
+        public async Task<IActionResult> OnPost()
+        {
+         if (Input.Button != "register")
+         {
+             return Redirect(Input.ReturnUrl ?? "~/");      
+         }
+           if(ModelState.IsValid)
+           {
+             var user = new ApplicationUser 
+             { 
+                 UserName = Input.Username, 
+                 Email = Input.Email,
+                 Fullname = Input.Fullname,
+                 EmailConfirmed = true
+             };
+             var result = await _usermanager.CreateAsync(user, Input.Password);
+             if (result.Succeeded)
+             {
+                await _usermanager.AddClaimAsync(user, 
+                new Claim( JwtClaimTypes.Name, Input.Fullname ?? ""));
+                 RegisterSuccess = true;
+                
+             }
+             foreach (var error in result.Errors)
+             {
+                 ModelState.AddModelError(string.Empty, error.Description);
+             }
+            
+           }
+             return Page();
+         }
+    }
---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Account.Register;
+
+    public class RegisterViewModel
+{ 
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set;}
+        [Required]
+        public string Username { get; set;}
+        [Required]
+        public string Fullname { get;}
+        public string ReturnUrl { get;}
+        public string Button { get;}
+}
+
