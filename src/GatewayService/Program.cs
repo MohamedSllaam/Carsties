904bc753f90358f015a85cf7a6d41f5fbb93ba76
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2026-01-29 01:13:33 +0200

    add MapReverseProxy

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+   .AddJwtBearer(option => {
+       option.Authority = builder.Configuration["IdentityServiceUrl"];
+       option.RequireHttpsMetadata = false;
+       option.TokenValidationParameters.ValidateAudience = false;
+       option.TokenValidationParameters.NameClaimType = "username";
+   });
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapReverseProxy();
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
